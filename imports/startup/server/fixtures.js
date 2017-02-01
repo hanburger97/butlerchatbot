fe153249493771b24ad6bc71eee072662e571718
Elic Ng <elicng@gmail.com>
2017-02-01 01:03:04 -0500

    Force fixture to update

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -5,11 +5,12 @@ import {Migrations} from 'meteor/percolate:migrations'
  */
 
 Migrations.add({
-  version: 1,
+  version: 2,
   up: function () {
     const postbacks = JSON.parse(Assets.getText('fixtures/postbacks_cleaned.json'))
 
     const postbacksCollection = new Meteor.Collection('postbacks', {_suppressSameNameError: true})
+    postbacksCollection.remove({})
     postbacks.forEach((postback) => {
       postbacksCollection.insert(postback)
     })
@@ -17,6 +18,7 @@ Migrations.add({
     const responses = JSON.parse(Assets.getText('fixtures/responses_cleaned.json'))
 
     const responsesCollection = new Meteor.Collection('responses', {_suppressSameNameError: true})
+    responsesCollection.remove({})
     responses.forEach((response) => {
       responsesCollection.insert(response)
     })
@@ -24,6 +26,7 @@ Migrations.add({
     const rooms = JSON.parse(Assets.getText('fixtures/rooms_cleaned.json'))
 
     const roomsCollection = new Meteor.Collection('rooms', {_suppressSameNameError: true})
+    roomsCollection.remove({})
     rooms.forEach((room) => {
       roomsCollection.insert(room)
     })
