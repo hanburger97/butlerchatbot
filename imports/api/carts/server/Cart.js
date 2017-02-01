80b32e0f457671f60d43281ff2883f899a60d0e5
Elic Ng <elicng@gmail.com>
2017-02-01 01:03:20 -0500

    rename

--- imports/api/carts/server/Cart.js
+++ imports/api/carts/server/Cart.js
@@ -8,19 +8,15 @@ class Cart {
     this.shopifyCart = shopifyCart
   }
 
-  addProduct (product, quantity = 1) {
+  addProductVariant (product, quantity = 1) {
     const _this = this
     return this.shopifyCart.createLineItemsFromVariants({variant: product, quantity})
       .then(shopifyCartModel => _this)
   }
 
-  updateProductQuantity (product, quantity) {
-    return this.updateProductIdQuantity(product.id, quantity)
-  }
-
-  updateProductIdQuantity (productId, quantity) {
+  updateVariantQuantity (variantId, quantity) {
     const _this = this
-    return this.shopifyCart.updateLineItem(firstLineItemId, 2)
+    return this.shopifyCart.updateLineItem(variantId, quantity)
       .then(shopifyCartModel => _this)
   }
 
@@ -30,11 +26,11 @@ class Cart {
       .then(shopifyCartModel => _this)
   }
 
-  removeProduct (product) {
+  removeProductVariant (product) {
     return this.removeProductId(product.id)
   }
 
-  clearAllProducts () {
+  clearAllVariants () {
     const _this = this
     return this.shopifyCart.clearLineItems()
       .then(shopifyCartModel => _this)
